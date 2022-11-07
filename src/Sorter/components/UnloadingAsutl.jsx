e9638a6de8c184a1e9e56c579acabe18ad8180f7
skladlista <skladlista@mail.ru>
2022-11-07 12:51:35 +0300

    fix styles

--- src/Sorter/components/UnloadingAsutl.jsx
+++ src/Sorter/components/UnloadingAsutl.jsx
@@ -1,18 +1,16 @@
 export default function UnloadingAsutl() {
-  return (
-    <div id="unloading__asutl" className="article unloading__asutl">
-      <p className="article__title">ВЫГРУЗКА ВАГОНОВ В ПРОГРАММЕ АСУ ТЛ</p>
-      <div className="article__subtitle article__div">
-        <ol>
-          <li>
-            Зайти в АСУТЛ &#8594; Функции &#8594; Вагоны на путях предприятия
-            &#8594; Найти и выбрать ПУТЬ (наш ПУТЬ 5) &#8594; В окне ниже
-            появятся вагоны &#8594; Надо выбрать все и нажать ВЫГРУЗКА ГРУЗА ИЗ
-            ВАГОНОВ &#8594; В появившемся окне нажать НАЧАТЬ ВЫГРУЗКУ и потом
-            ЗАВЕРШИТЬ ВЫГРУЗКУ.
-          </li>
-        </ol>
-      </div>
-    </div>
-  );
+    return (
+        <div id="unloading__asutl" className="article unloading__asutl">
+            <p className="article__title">ВЫГРУЗКА ВАГОНОВ В ПРОГРАММЕ АСУ ТЛ</p>
+            <div className="article__subtitle article__div">
+
+                &middot; Зайти в АСУТЛ &#8594; Функции &#8594; Вагоны на путях предприятия
+                &#8594; Найти и выбрать ПУТЬ 5
+                <br/>&middot; В окне ниже
+                появятся вагоны &#8594; Надо выбрать все и нажать ВЫГРУЗКА ГРУЗА ИЗ
+                ВАГОНОВ <br/> &middot; В появившемся окне нажать НАЧАТЬ ВЫГРУЗКУ и потом
+                ЗАВЕРШИТЬ ВЫГРУЗКУ.
+            </div>
+        </div>
+    );
 }
